b0e8286a2a383f0557006fcf2a91d4d35eba99e8
Richa Parikh <richa.parikh@mobilelive.ca>
2024-07-02 13:32:15 -0400

    refactor API with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT =3000;
@@ -18,13 +19,14 @@ app.get('/todos', function(req, res){
 
 app.get('/todos/:id', function(req, res){
     var todoID = parseInt(req.params.id);
-    var matchedTodo;
+    var matchedTodo = _.findWhere(todos, {id: todoID});
+    // var matchedTodo;
 
-    todos.forEach( todo => {
-        if (todoID === todo.id) {
-            matchedTodo = todo;
-        }
-    })
+    // todos.forEach( todo => {
+    //     if (todoID === todo.id) {
+    //         matchedTodo = todo;
+    //     }
+    // })
 
     if(matchedTodo){
         res.json(matchedTodo);
@@ -36,9 +38,13 @@ app.get('/todos/:id', function(req, res){
 });
 
 app.post('/todos', function(req, res) {
-    var body = req.body;
+    var body = _.pick(req.body, 'description','completed');
+    var trimmed = body.description.trim();
+    if (!_.isBoolean(body.completed) || !_.isString(body.description) || trimmed.length === 0) {
+        return res.status(400).send()
+    }
     body.id = todoNextId++;
-    
+    body.description = body.description.trim();
     todos.push(body)
  
     res.json(todos);
