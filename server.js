4d2c9a5da820ca7f39d941fb1bb13ac434ed495f
Richa Parikh <richa.parikh@mobilelive.ca>
2024-07-02 11:51:32 -0400

    add POST to todos

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT =3000;
-var todos = [{
-    id:1,
-    description: 'Meet Shalin for dinner',
-    completed: false
-}, {
-    id: 2,
-    description: 'Buy milk for Rishi',
-    completed: false
-}, {
-    id: 3,
-    description: 'Cook dinner',
-    completed: true
-}]
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res){
     res.send('Todo API Root');
@@ -40,6 +33,15 @@ app.get('/todos/:id', function(req, res){
     }
         
     // res.send('asking for todo with id of '+req.params.id);
+});
+
+app.post('/todos', function(req, res) {
+    var body = req.body;
+    body.id = todoNextId++;
+    
+    todos.push(body)
+ 
+    res.json(todos);
 })
 
 app.listen(PORT, function(){
