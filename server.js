8ff73070da502c25bbe321b70b4de03857f93947
Richa Parikh <richa.parikh@mobilelive.ca>
2024-07-03 09:50:40 -0400

    add ?completed filter

--- server.js
+++ server.js
@@ -14,7 +14,19 @@ app.get('/', function(req, res){
 })
 
 app.get('/todos', function(req, res){
-    res.json(todos);
+    var queryParams = req.query;
+    console.log(queryParams);
+    var filteredTodos = todos;
+    console.log(queryParams.completed === 'true', queryParams.hasOwnProperty('completed') )
+
+    if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true' ) {
+        
+        filteredTodos = _.where(filteredTodos, {completed: true})
+    } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+        filteredTodos = _.where(filteredTodos, {completed: false})
+    }
+
+    res.json(filteredTodos);
 })
 
 app.get('/todos/:id', function(req, res){
